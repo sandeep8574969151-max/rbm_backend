bdeedb8f3d4a8b8a49ef8d2742243d674e5ec9e6
Sandeep <aapka-email@example.com>
2026-07-04 14:59:54 +0530

    Moved files to root

--- add_banner.php
+++ add_banner.php
@@ -1,4 +1,10 @@
-?php include 'db.php'; ?>
+<?php
+// Sabhi domains ko allow karein
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+include 'db.php'; ?>
 <form action="upload_banner.php" method="POST" enctype="multipart/form-data">
     <h3>Add New Banner</h3>
     Title: <input type="text" name="title" required><br><br>
@@ -6,3 +12,4 @@
     Link URL: <input type="text" name="linkUrl" placeholder="/products"><br><br>
     <button type="submit" name="submit">Upload Banner</button>
 </form>
+?>
